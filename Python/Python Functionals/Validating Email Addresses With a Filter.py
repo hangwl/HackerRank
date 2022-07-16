cc1ff0370fc5b3019935c68e06756192342aaaef
hangwl <hangwei54e@hotmail.com>
2022-07-16 21:44:02 +0800

    re

--- Python/Python Functionals/Validating Email Addresses With a Filter.py
+++ Python/Python Functionals/Validating Email Addresses With a Filter.py
@@ -7,11 +7,11 @@
 import re # regex
 
 def fun(s):       
-    a = re.match(r'[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$',s)
+    a = re.match('[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$',s) # $ matches the end of the string; {1,3} matches 1 to 3 characters for previous RE [a-zA-Z] 
     return(a)
 
 def filter_mail(emails):
-    return list(filter(fun, emails))
+    return list(filter(fun, emails)) # filter(function, iterable) returns iterator if function returns True for iterator
 
 if __name__ == '__main__':
     n = int(input())
