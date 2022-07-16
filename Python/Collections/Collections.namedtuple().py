5cb8d9f2d8524f209dd17421cb3c68749cd1861f
hangwl <hangwei54e@hotmail.com>
2022-07-16 17:14:48 +0800

    namedtuple

--- Python/Collections/Collections.namedtuple().py
+++ Python/Collections/Collections.namedtuple().py
@@ -2,8 +2,8 @@
 They turn tuples into convenient containers for simple tasks.
 With namedtuples, you don’t have to use integer indices for accessing members of a tuple."""
 
-
 from collections import namedtuple
+
 N = int(input())
 entry = namedtuple('entry', ','.join(input().split()))
 print(f'{sum([int(entry(*input().split()).MARKS) for i in range(N)])/N:.2f}')
